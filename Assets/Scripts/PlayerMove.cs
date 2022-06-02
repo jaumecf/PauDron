26a28c66e8d5d24f631ffd8a31cb7018278eda99
xescaalorda <avf@paucasesnovescifp.cat>
2022-06-02 11:30:19 +0200

    afegir rotació i convertir player a prefab

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    float hInput, vInput;
+    float hInput, vInput, deltaX, deltaY;
     Rigidbody playerRb;
     [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float rotateSpeed = 1f;
 
     // Variables Bateria
     float batteryCharge;
@@ -28,7 +29,6 @@ public class PlayerMove : MonoBehaviour
             GameObject.Find("SceneManager").GetComponent<SceneManager>().pasaObstaculo();
             
         } 
-
     }
 
     // Update is called once per frame
@@ -36,14 +36,28 @@ public class PlayerMove : MonoBehaviour
     {
         hInput = Input.GetAxis("Horizontal"); //D A eix x
         vInput = Input.GetAxis("Vertical"); //W S eix z
+
         playerRb.MovePosition(transform.position + (transform.right * hInput + transform.forward * vInput) * moveSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.Q))
+
+        if (Input.GetKey(KeyCode.Q)) //eix y puja
         {
             playerRb.MovePosition(transform.position + transform.up * moveSpeed * Time.deltaTime);
         }
-        else if (Input.GetKey(KeyCode.E))
+        else if (Input.GetKey(KeyCode.E)) //eix y baixa
         {
             playerRb.MovePosition(transform.position + transform.up * (-moveSpeed) * Time.deltaTime);
+        }
+
+        //rota dreta i esquerra sobre eix y
+        deltaX = Input.GetAxis("Mouse X");
+        Vector3 rotationAngle = Vector3.up * deltaX * rotateSpeed;
+        Quaternion rotation = Quaternion.Euler(rotationAngle);
+        playerRb.MoveRotation(playerRb.rotation * rotation);
+
+        //per evitar que es segueixi movent quan col·lisiona
+        if (haveInertia())
+        {
+            Invoke("stopPlayer", 2f);
         }
 
         // Actualització de la bateria
@@ -59,5 +73,21 @@ public class PlayerMove : MonoBehaviour
             // Aturar bitxo!
             // Menu reiniciar joc?
         }
+    }
+
+    private bool haveInertia ()
+    {
+        if (Mathf.Abs(vInput) < 0.01f && Mathf.Abs(hInput) < 0.01f && Mathf.Abs(deltaX) < 0.01f && 
+            !Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.E) && 
+            Mathf.Abs(playerRb.velocity.x) > 0.01f && Mathf.Abs(playerRb.velocity.y) > 0.01f && Mathf.Abs(playerRb.velocity.z) > 0.01f)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void stopPlayer()
+    {
+        playerRb.velocity = Vector3.zero;
     }
 }
